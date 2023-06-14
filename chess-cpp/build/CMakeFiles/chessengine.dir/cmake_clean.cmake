17890a786f2566e77dd21d0595f91f037df1fa6f
yanis belami <yanis.belami@epita.fr>
2023-06-14 23:38:57 +0200

    test

---
+++ chess-cpp/build/CMakeFiles/chessengine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chessengine.dir/src/main.cc.o"
+  "CMakeFiles/chessengine.dir/src/main.cc.o.d"
+  "chess"
+  "chess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chessengine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
